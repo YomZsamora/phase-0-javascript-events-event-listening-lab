61e25b2100d70391b3e0eaae283eb726880eae3f
yomZsamora <samora.y@adzumi.co.ke>
2022-02-06 13:24:32 +0300

    Completed Lab.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+
 const input = document.querySelector('input');
 
+// Binds an event listener in addingEventListener()
 function addingEventListener() {
 	input.addEventListener('click', function(event) {
 		alert('I was clicked!');
